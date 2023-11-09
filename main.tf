1bb485bff44e3754574fce4a7aa364a61477f2fd
Felipe Alecsandro <felipe.alecsandro@hotmail.com>
2023-11-08 21:57:52 -0300

    adjust main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-  }
-  required_version = ">= 1.1.0"
-
   cloud {
     organization = "challenge-fiap"
 
